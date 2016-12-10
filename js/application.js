035daa5d1803c880e0d5bfc6a251605fb218f20d
kboygit <heybisayangdako@gmail.com>
2016-12-09 17:21:09 -0800

    modified loop

--- js/application.js
+++ js/application.js
@@ -23,3 +23,43 @@ function startSlider() {
     sliderNext = sliderNext + 1;
   }, 3000)
 }
+
+function prev() {
+  newSlide = sliderInt - 1;
+  showSlide(newSlide);
+}
+
+function next() {
+  newSlide=sliderInt + 1;
+  showSlide(newSlide);
+}
+
+function stopLoop() {
+  window.clearInterval(loop);
+}
+
+function showSlide(id) {
+  stopLoop();
+  if (id>count) {
+    id =1;
+  }else if(id<1){
+    id = count;
+  }
+
+  $("#slider > img").fadeOut(300);
+  $("#slider > img#" + sliderNext).fadeIn(300);
+
+  sliderInt = id;
+  sliderNext = id + 1;
+  startSlider();
+}
+
+$('#slider > img').hover(
+function(){
+	stopLoop();
+},
+function(){
+	startSlider();
+
+	}
+)
